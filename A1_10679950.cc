e181d76cf3e3083377e89b8f94c1d304815c23bc
Amenyiwah <42740048+Amenyiwah@users.noreply.github.com>
2018-08-29 11:32:20 -0700

    Update A1_10679950.cc
    Done with assignment

--- A1_10679950.cc
+++ A1_10679950.cc
@@ -1,1 +1,28 @@
-
+# include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	int prime;
+	
+	cout <<"Please enter number" << endl;
+	cin >> num;
+	
+if(num==2||num==3||num==5||num==7||num==11){
+	cout<<"Number is a prime number"<<endl;
+}else{
+	if(num%2!=0){
+		if(num%3!=0){
+			if(num%5!=0){
+				if(num%7!=0){
+					if(num%11!=0){
+						cout<<"Number is a prime number";
+					} else cout<<"Not prime";
+				}else cout<<"Not prime";
+			}else cout<<"Not prime";
+		}else cout<<"Not prime";
+	}else cout<<"Not prime";
+}
+	return 0;
+	
+}
